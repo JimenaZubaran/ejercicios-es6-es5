a9cfd5e0789a0c19e301901043a9b631e624e6de
JimenaZubaran <jimenab.zubaran@gmail.com>
2018-02-21 20:50:45 -0600

    ejercicio3

--- 3.-inventory/js/app.js
+++ 3.-inventory/js/app.js
@@ -15,13 +15,15 @@ const inventory = [
   { name: "Ciruela", category: "fruit", stock: "8" }
 ];
 
-var totalByCategory = function(inventory, category) {
+/*var totalByCategory = function(inventory, category) {
   var filterCategory = function filterCategory(prod) {
     var totalProd = prod.category === category;
+    console.log(totalProd);
     return totalProd;
   };
   var sumInventory = function sumInventory(prodAnterior, prodActual) {
     var base = prodAnterior + parseInt(prodActual.stock, 10);
+    console.log(base);
     return base;
   };
   var filtrando = inventory.filter(filterCategory).reduce(sumInventory, 0);
@@ -29,8 +31,27 @@ var totalByCategory = function(inventory, category) {
   return filtrando;
 };
 
-totalByCategory(inventory, "beverage");
+totalByCategory(inventory, "beverage");*/
 
 
 // Comenta todo el código de arriba (ecepto la const inventory) y pasa el código a ES6
 
+
+const totalByCategory = (inventory,category) =>{
+  const filterCategory = prod => {
+    let totalProd = prod.category === category;
+    return totalProd;
+  };
+
+  const sumInventory = (prodAnterior, prodActual)  => {
+    let base = prodAnterior + parseInt(prodActual.stock, 10);
+    return base;
+  };
+
+  const filtrando = inventory.filter(filterCategory).reduce(sumInventory,0);
+  console.log(filtrando);
+  return filtrando;
+
+};
+
+totalByCategory(inventory, "beverage");
