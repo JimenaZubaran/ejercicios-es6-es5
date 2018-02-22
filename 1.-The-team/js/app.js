d3ae919e879f5e87c6e8142ada134daae4105440
JimenaZubaran <jimenab.zubaran@gmail.com>
2018-02-21 18:58:01 -0600

    ejercicio1

--- 1.-The-team/js/app.js
+++ 1.-The-team/js/app.js
@@ -1,15 +1,15 @@
-         
-         
+
+
          // ES5
 
 // declaracion de las variables
 
-var [teachersTeam, members] = [
+/*var [teachersTeam, members] = [
   'formacion',
      ['Amalia',
-      'Karla', 
-      'Silvana', 
-      'Manu', 
+      'Karla',
+      'Silvana',
+      'Manu',
       'Jonh',
       'Mike'
     ],
@@ -19,19 +19,36 @@ var formatTeamMessage = function (teachersTeam, members) {
   var message = 'Hay ' + members.length + " " + 'personas en el equipo de' + " " + teachersTeam + "\n" + "Ellos son" + " " + members.join(", ") + ".";
   console.log(message)
   return message;
-};  
+};
 
 formatTeamMessage(teachersTeam, members);
-
+*/
 // Ahora comenta todo el código anterior y escribelo en ES6
 
 
                             //  ES6
-// declaracion de las variables 
+// declaracion de las variables
+const [teachersTeam, members] = [
+  'formacion',
+     ['Amalia',
+      'Karla',
+      'Silvana',
+      'Manu',
+      'Jonh',
+      'Mike'
+    ],
+];
 
 // const [teamName, product] = [
 //   'formacion', ['Amalia', 'Karla', 'Silvana', 'Manu', 'Jonh', 'Mike'],
 // ];
+
+const formatTeamMessage = (teachersTeam, members) => {
+  return `Hay ${members.length} personas en el equipo de ${teachersTeam}.
+  Ellos son: ` + members.join(", ");
+}
+console.log(formatTeamMessage(teachersTeam, members));
+
 
 
 // const formatTeamMessage = (teamName, people) => {
