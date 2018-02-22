a1e07434102872ff48a67d9d041cc21f12a1420c
JimenaZubaran <jimenab.zubaran@gmail.com>
2018-02-21 21:44:36 -0600

    ejercicio5

--- 5.-Login/js/app.js
+++ 5.-Login/js/app.js
@@ -1,19 +1,38 @@
-// En este ejercicio deberas crear tu solción en ES5, asegurarte que funcione 
+// En este ejercicio deberas crear tu solción en ES5, asegurarte que funcione
 
 // declara el prompt
- 
-function user () {
+ /*var password = "laboratoria";
+function user (string) {
 // escribe tu código aqui...
+  var passwordQuestion = prompt("¿Cuál es tu contraseña?");
+    if(passwordQuestion === "laboratoria"){
+      console.log("verdadero");
+    } else if (passwordQuestion !== "laboratoria") {
+      console.log("falso");
+    }else{
+      console.log(falso);
+    }
 }
 
 
-
-
+user();
+*/
 
 
 // comenta todo lo anterior y escribelo en ES6
 
-
+const password = "laboratoria";
+const user = string => { //function expression
+  const passwordQuestion = prompt("¿Cuál es tu contraseña?");
+  if(passwordQuestion === "laboratoria"){
+    console.log("verdadero");
+  } else if (passwordQuestion !== "laboratoria") {
+    console.log("falso");
+  }else{
+    console.log(falso);
+  }
+}
+user();
 // user () => {
 
 // }
