940fdc1793340aaca320f82d9f2c27138a2e9668
JimenaZubaran <jimenab.zubaran@gmail.com>
2018-02-21 20:14:12 -0600

    ejercicio2

--- 2.-squareOfPairs/js/app.js
+++ 2.-squareOfPairs/js/app.js
@@ -4,23 +4,55 @@
 
 // declara las variables ES5
 
-var array = [2, 4, 6];
-// var array = [-3, 2, -8, 12, 5];
-// var array = [1, 2, 3, 4, 5];
+/*var array = [2, 4, 6];
+//var array = [-3, 2, -8, 12, 5];
+//var array = [1, 2, 3, 4, 5];
 
 
-// declaración de la funcion 
+// declaración de la funcion
 
-function square () {
+function square (element) {
+  var even = array.map(function(item){
+    if(item > 0){
+      return item * item
+    }else{
+      return item
+    }
+  })
+return even;
   // escribe tu código aqui...
-}
+};
 
-square(array);
+console.log(square(array));*/
+
+
+
+// escribe tu código aqui...
+
+//console.log(even);
 
 
                       //  ES6
 
 // Ahora comenta todo el codigo de arriba y escribelo en ES6
+const numbers = [2, 4, 6];
+const  numbersOne= [-3, 2, -8, 12, 5];
+const numbersTwo = [1, 2, 3, 4, 5];
+
+const square = array => {
+  return  array.map(item => {
+    if(item > 0){
+      return item * item
+    } else {
+       return item;
+    }
+  });
+}
+console.log(square(numbers));
+console.log(square(numbersOne));
+console.log(square(numbersTwo));
+
+
 
 
 // declara variables (let-const)
@@ -36,5 +68,4 @@ square(array);
 
 
 
-// ejecuta la funcion 
-
+// ejecuta la funcion
